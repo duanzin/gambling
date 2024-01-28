a3a5b1824bef1e0224439871de908d306e363e9d
duanzin <lucaduan@gmail.com>
2024-01-28 12:03:57 -0300

    fix: prevents strings from being all spaces

--- src/schemas/gameSchema.ts
+++ src/schemas/gameSchema.ts
@@ -1,8 +1,8 @@
 import Joi from "joi";
 
 export const createGameSchema = Joi.object({
-  homeTeamName: Joi.string().min(1).regex(/^\S/).required(),
-  awayTeamName: Joi.string().min(1).regex(/^\S/).required(),
+  homeTeamName: Joi.string().min(1).regex(/^\S.*\S$/).required(),
+  awayTeamName: Joi.string().min(1).regex(/^\S.*\S$/).required(),
 });
 
 export const endGameSchema = Joi.object({
--- src/schemas/participantSchema.ts
+++ src/schemas/participantSchema.ts
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
 export const createParticipantSchema = Joi.object({
-  name: Joi.string().min(1).regex(/^\S/).required(),
+  name: Joi.string().min(1).regex(/^\S.*\S$/).required(),
   balance: Joi.number().integer().min(1000).required(),
 });
