29751bb4542eb7a1bb42cb551415a59539b0a482
duanzin <lucaduan@gmail.com>
2024-01-31 22:06:40 -0300

    fix: correctly gets betsController

--- src/routes/betsRouter.ts
+++ src/routes/betsRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { createBetSchema } from "../schemas/betSchema";
 import { validateSchema } from "../middleware/validateSchema";
-import betsController from "controller/betsController";
+import betsController from "../controller/betsController";
 
 const betsRouter = Router();
 
