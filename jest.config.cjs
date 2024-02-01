e543e8d2a60a98e758756187a8053df551608a56
duanzin <lucaduan@gmail.com>
2024-01-31 21:42:56 -0300

    feat: added coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   testEnvironment: "node",
   moduleDirectories: ["node_modules", "src"],
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   testMatch: ["<rootDir>/tests/**/*.(test|spec).ts" ],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
